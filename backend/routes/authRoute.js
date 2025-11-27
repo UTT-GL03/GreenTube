72aab5a18c6f902506d2304528691d6833a72392
HugoCnk <czerniakhugo@gmail.com>
2025-11-27 19:23:47 +0100

    FEATURE: Add backend simple login

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,49 @@
+import express from "express";
+
+export default function (db) {
+
+    const router = express.Router();
+
+    router.post("/login", async (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        try {
+            const result = await db.find({
+                selector: {
+                    type: "user",
+                    password: password,
+                    $or: [
+                        { pseudo: username },
+                        { email: username }
+                    ]
+                },
+                limit: 1
+            })
+
+            if (result.docs.length === 0) {
+                return res.status(401).json({ error: "Invalid username/email or password." });
+            }
+
+            const user = result.docs[0];
+
+            res.json({
+                success: true,
+                message: "Login successful",
+                user: {
+                    _id: user._id,
+                    pseudo: user.pseudo,
+                    email: user.email,
+                    avatar: user.avatar
+                }
+            });
+        } catch (err) {
+            console.error("Login error:", err);
+            res.status(500).json({ error: "Internal server error" });
+        }
+    })
+
+    router.post("/signUp", (req, res) => {
+
+    })
+    return router;
+}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import express from "express";
+import cors from "cors"
+import nano from "nano"
+import authRoute from "./routes/authRoute.js"
+import uploadRoute from "./routes/uploadRoute.js"
 
-app.get("/", (req, res) => {
-  res.send("BACKEND OK");
+const DB_URL = "http://tbhc:tbhc@couchdb:5984"
+const PORT = 3000;
+
+const app = express();
+const couch = nano(DB_URL)
+const db = couch.db.use("greentube");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoute(db))
+app.use("/upload", uploadRoute(db))
+
+app.get("/", async (req, res) => {
+  try {
+    const info = await db.info();
+    res.json(info);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Serveur lancé sur http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
