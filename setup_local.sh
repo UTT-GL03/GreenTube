333513876c631a3328d6296a2e22f7785215b49e
HugoCnk <czerniakhugo@gmail.com>
2025-12-01 18:31:51 +0100

    CHORE: Improving setup_local file

--- setup_local.sh
+++ setup_local.sh
@@ -1,9 +1,19 @@
+if ! docker info >/dev/null 2>&1; then
+    echo "Docker Desktop n'est pas lancé. Merci d'ouvrir Docker Desktop puis de relancer le script."
+    exit 1
+fi
+
+echo "Installation des dépendances frontend..."
 cd frontend/
 npm install
 
+echo "Installation des dépendances backend..."
 cd ../backend/
 npm install 
 
+echo "Lancement des containers Docker..."
 export COUCHDB_USER="tbhc"
 export COUCHDB_PASSWORD="tbhc"
 docker compose up -d
+
+echo "Projet local initialisé avec succès !"
