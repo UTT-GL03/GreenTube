0a877ace41a6bc3de48a3d531bef848670c67ec5
HugoCnk <czerniakhugo@gmail.com>
2025-12-15 14:27:47 +0100

    REFACTOR: Change max video size

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -6,7 +6,7 @@ export const APP = {
   NAME: "GreenTube",
   DEFAULT_AVATAR: "uploads/avatars/default_avatar_1.png",
   VIDEO_LIMIT: 6,
-  VIDEO_MAX_SIZE: 200,
+  VIDEO_MAX_SIZE: 5000,
   THUMBNAIL_MAX_SIZE: 50,
   AVATAR_MAX_SIZE: 10,
 };
